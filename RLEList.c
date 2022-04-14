a3361946984d9278b66cf343752b874b8fd836b4
noyzini <102484852+noyzini@users.noreply.github.com>
2022-04-14 11:57:38 +0300

    added RLEListCreate func

--- RLEList.c
+++ RLEList.c
@@ -1,10 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "RLEList.h"
 
-typedef struct RLEList_t{
+#define EMPTY '\0'
+
+ struct RLEList_t{
     char letter;
-    int reptitions;
+    int repetitions;
     struct RLEList_t* next;
 };
 
 //implement the functions here
-RLEList RLEListCreate();
+RLEList RLEListCreate(){
+    RLEList ptr= malloc(sizeof(*ptr));
+    if(!ptr)
+    {
+        return NULL;
+    }
+    ptr ->letter=EMPTY;
+    ptr ->repetitions=0;
+    ptr ->next =NULL;
+    return ptr;
+}
