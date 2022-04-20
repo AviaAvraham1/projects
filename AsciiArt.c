5a142b946384440f3fb273cdecd5747719a16a35
AviaAvraham1 <avia.avraham@campus.technion.ac.il>
2022-04-20 13:54:34 +0300

    main functions

--- AsciiArt.c
+++ AsciiArt.c
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 
 
-
 RLEList asciiArtRead(FILE* in_stream)
 {
     RLEList file_RLE = RLEListCreate();
@@ -28,18 +27,18 @@ RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
 {
     RLEListResult result;
     char *list_as_string = RLEListExportToString(list,&result);
-    char *write = malloc(RLEListSize(list) * sizeof(char)); //free this
-    char *ptr = write;
-    char letterToWrite = 'A';
-    int letterRepetitions = 0;
 
     if (result != RLE_LIST_SUCCESS)//test this!!! result might be initialized as NULL ?
         return result;
 
+    char *write = malloc(RLEListSize(list) * sizeof(char)); //need better name
+    char *ptr = write;
+    char letterToWrite = 'A';
+    int letterRepetitions = 0;
 
     while (*list_as_string)
     {
-        char currentChar = list_as_string[0];
+        char currentChar = *list_as_string;
 
         if (currentChar >= '0' && currentChar <= '9')
         {
@@ -48,13 +47,15 @@ RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
         }
         else if (currentChar == '\n')
         {
-            if (letterToWrite == currentChar)
-                letterRepetitions--;
+            //if (letterToWrite == currentChar)
+            //    letterRepetitions--;
+
             for (int i = 0; i < letterRepetitions; i++)
             {
                 *ptr = letterToWrite;
                 ptr++;
             }
+
             letterToWrite = '\n';
             letterRepetitions = 0;
         }
@@ -65,6 +66,8 @@ RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
         list_as_string++;
     }
     fputs(write,out_stream);
+    free(write);
+    free(list_as_string);
     return RLE_LIST_SUCCESS;
 }
 
@@ -75,8 +78,11 @@ RLEListResult asciiArtPrintEncoded(RLEList list,FILE* out_stream)
     {
         return RLE_LIST_NULL_ARGUMENT;
     }
+
     RLEListResult result;
     char *encoded_list= RLEListExportToString(list,&result);
+    if (result != RLE_LIST_SUCCESS)
+        return result;
     fprintf(out_stream,"%s" ,encoded_list);
     free(encoded_list);
     return RLE_LIST_SUCCESS;
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //#include "RLEList.h"
 #include "AsciiArt.h"
@@ -10,10 +11,11 @@
  * on RLEListRemove() join 2 nodes together if middle one is gone
  * complete RLEList.c's to do list
  */
+char InvertChar(char to_invert);
 
 int main(int argc, char** argv)
 {
-    char *readPath = "../test2.txt", *writePath = "../writeHere.txt";
+    char *readPath = "../test1.txt", *writePath = "../writeHere.txt";
     FILE* fileReadFrom = fopen(readPath, "r");
     RLEList fileAsList = asciiArtRead(fileReadFrom);
     RLEListResult result;
@@ -22,7 +24,8 @@ int main(int argc, char** argv)
 
     FILE* fileWriteTo = fopen(writePath,"w");
     //FILE* out = stdout; //doesn't work for some reason
-    result = asciiArtPrint(fileAsList,fileWriteTo);
+    //result = asciiArtPrint(fileAsList,fileWriteTo);
+    result = asciiArtPrintEncoded(fileAsList,fileWriteTo);
 
     if (result == RLE_LIST_ERROR)
         printf("error");
@@ -33,13 +36,56 @@ int main(int argc, char** argv)
     //-------AsciiArtTool - code ---------------
     if(argc!=4) //needs a define
     {
-        printf("Error in input, not enough arguments");
+        printf("Usage: ./AsciiArtTool <flags> <source> <target>");
         return 0;
     }
+    else
+    {
+        FILE* source = fopen(argv[2],"r");
+        if (!source)
+        {
+            printf("error");
+            return 0;
+        }
+        FILE* dest = fopen(argv[3],"w");
+        if (!dest)
+        {
+            printf("error");
+            fclose(source);
+            return 0;
+        }
 
+        RLEList image = asciiArtRead(source);
 
-
+        if (strcmp(argv[1],"-e") == 0)
+        {
+            asciiArtPrintEncoded(image,dest);
+            RLEListDestroy(image);
+        }
+        else if (strcmp(argv[1],"-i") == 0)
+        {
+            RLEListMap(image,InvertChar);
+            asciiArtPrint(image,dest);
+            RLEListDestroy(image);
+        }
+        else
+        {
+            printf("Unknown flag");
+            return 0;
+        }
+        fclose(source);
+        fclose(dest);
+    }
     return 0;
+}
+
+char InvertChar(char to_invert)
+{
+    if (to_invert == ' ')
+        return '@';
+    if (to_invert == '@')
+        return ' ';
+    return to_invert;
 }
 
 
